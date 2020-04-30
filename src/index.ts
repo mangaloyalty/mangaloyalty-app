12fec2c6e375b8a66166f70de4c713aa81c6bf8a
Deathspike <>
2020-04-30 19:43:58 +0200

    Initial commit using mangaloyalty-client as base

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './boot';
+export * from './contexts/LibraryContext';
+export * from './contexts/RemoteContext';
+export * from './contexts/SessionContext';
+export * from './contexts/SocketContext';
+export * from './managers/BrowserManager';
+export * from './managers/ResourceManager';
