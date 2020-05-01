fa17b9d2684470c77eef596addc9a15a76190248
Roel van Uden <r.h.g.v.uden@outlook.com>
2020-05-01 21:04:17 +0200

    Mock 0.12.0-b2 core trace, boot sv & android mocks

---
+++ app/App.Platform.Android/Plugins/BrowserPlugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using App.Core.Models.Plugins;
+using App.Core.Plugins;
+using Newtonsoft.Json.Linq;
+
+namespace App.Platform.Android.Plugins
+{
+    public sealed class BrowserPlugin : IBrowserPlugin
+    {
+        public Task BootAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<string> CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DestroyAsync(BrowserDataModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JToken> EvaluateAsync(BrowserEvaluateDataModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateAsync(BrowserNavigateDataModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ResponseAsync(BrowserResponseDataModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WaitForNavigateAsync(BrowserDataModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ app/App.Platform.Android/Plugins/TracePlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using App.Core.Models.Plugins;
+using App.Core.Plugins;
+
+namespace App.Platform.Android.Plugins
+{
+    public sealed class TracePlugin : ITracePlugin
+    {
+        public Task InfoAsync(TraceDataModel model)
+        {
+            Console.WriteLine(model.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
