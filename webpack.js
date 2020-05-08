31bd966b6c0324755632f942345f454bcd395348
Deathspike <meister.deathspike@outlook.com>
2020-05-08 21:40:43 +0200

    Use prebuilt client libraries of b4

--- webpack.js
+++ webpack.js
@@ -1,6 +1,6 @@
 module.exports = Object.assign(require('./webpack.resolve'), { 
-  entry: './dist/app',
-  output: {filename: 'app.min.js', path: `${__dirname}/public`},
+  entry: {client: './dist/client/app', server: './dist/server/app'},
+  output: {filename: '[name].min.js', path: `${__dirname}/public`},
   performance: {hints: false},
   mode: 'production'
 });
