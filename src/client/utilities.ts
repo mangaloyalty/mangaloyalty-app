26a1843e72eefbb7c7d9460b3cde640db97d8da5
Deathspike <>
2020-05-08 21:58:29 +0200

    TypeScript bridges to native

--- src/client/utilities.ts
+++ src/client/utilities.ts
@@ -1,3 +1,15 @@
-export function proxyAsync(eventSource: string, functionName: string, parameters: {[key: string]: boolean | number | string | undefined}) {
-  return window.oni?.sendAsync(`proxy.forwardAsync`, {eventSource, functionName, parameters}) as Promise<any>;
+export function proxyAsync(ev: {[key: string]: boolean | number | string | undefined}) {
+  return window.oni?.sendAsync(`proxy.forwardAsync`, ev) as Promise<any>;
+}
+
+export function toBlob(value: string) {
+  const a = [];
+  const b = atob(value);
+  for (let i = 0; i < b.length; i += 512) {
+    const s = b.slice(i, i + 512);
+    const v = new Array(s.length);
+    for (let j = 0; j < s.length; j++) v[j] = s.charCodeAt(j);
+    a.push(new Uint8Array(v));
+  }
+  return new Blob(a);
 }
