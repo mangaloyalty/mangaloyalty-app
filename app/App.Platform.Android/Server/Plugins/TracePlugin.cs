42af3e0a4c7d45b46da3b22cb3fd5990e01f4ad4
Deathspike <>
2020-05-09 08:33:30 +0200

    Add Android trace plugin.

--- app/App.Platform.Android/Server/Plugins/TracePlugin.cs
+++ app/App.Platform.Android/Server/Plugins/TracePlugin.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using App.Core.Server;
 using App.Core.Server.Models;
 
 namespace App.Platform.Android.Server.Plugins
 {
-    // TODO: Write the trace information to an accessible file.
     public class TracePlugin : ITracePlugin
     {
+        #region Abstracts
+
+        private static string GetAbsolutePath(string absolutePath)
+        {
+            var basePath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var relativePath = absolutePath.TrimStart('/');
+            return Path.Combine(basePath, relativePath);
+        }
+
+        #endregion
+
         #region Implementation of ITracePlugin
 
-        public Task InfoAsync(TraceDataModel model)
+        public async Task InfoAsync(TraceDataModel model)
         {
-            Console.WriteLine(model.Message);
-            return Task.CompletedTask;
+            await using var fs = File.Open(GetAbsolutePath(model.AbsolutePath), FileMode.Append);
+            await using var sw = new StreamWriter(fs);
+            await sw.WriteLineAsync($"{DateTime.Now:s}: {model.Message}");
+            await sw.FlushAsync();
         }
 
         #endregion
