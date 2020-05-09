5257a3fc0e404009baad28bc23d8d76614829409
Deathspike <>
2020-05-09 08:41:47 +0200

    Add Andrid event support for boot & replace

---
+++ app/App.Platform.Android/Server/Receivers/BootCompletedReceiver.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace App.Platform.Android.Server.Receivers
+{
+    [BroadcastReceiver]
+    [IntentFilter(new[] {Intent.ActionBootCompleted})]
+    public sealed class BootCompletedReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            ServerService.StartService(context);
+        }
+    }
+}
