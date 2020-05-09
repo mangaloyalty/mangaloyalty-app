2e2855eef56caa5bdf2249b0f977434e4a70712d
Deathspike <meister.deathspike@outlook.com>
2020-05-09 13:36:57 +0200

    Add click handlers for notifications.

---
+++ app/App.Platform.Android/Server/Receivers/NotificationReceiver.cs
@@ -0,0 +1,15 @@
+﻿using Android.Content;
+
+namespace App.Platform.Android.Server.Receivers
+{
+    [BroadcastReceiver]
+    public class NotificationReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            context.StartActivity(context.PackageManager
+                .GetLaunchIntentForPackage(context.PackageName)
+                .SetFlags(ActivityFlags.BroughtToFront | ActivityFlags.ReorderToFront | ActivityFlags.NewTask));
+        }
+    }
+}
