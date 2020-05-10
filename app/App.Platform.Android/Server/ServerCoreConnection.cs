c2f35179cd060b194c993321beab9785cc9a3eba
Deathspike <>
2020-05-10 08:50:52 +0200

    Fix browser cleanup, connection management & foreground notification

--- app/App.Platform.Android/Server/ServerCoreConnection.cs
+++ app/App.Platform.Android/Server/ServerCoreConnection.cs
@@ -11,17 +11,19 @@ namespace App.Platform.Android.Server
     public class ServerCoreConnection : Java.Lang.Object, IServerCore, IServiceConnection
     {
         private readonly TaskCompletionSource<ServerCoreBinder> _binderTcs;
+        private readonly Context _context;
 
         #region Constructor
 
-        private ServerCoreConnection()
+        private ServerCoreConnection(Context context)
         {
             _binderTcs = new TimeoutTaskCompletionSource<ServerCoreBinder>();
+            _context = context;
         }
 
         public static ServerCoreConnection Create(Context context)
         {
-            var connection = new ServerCoreConnection();
+            var connection = new ServerCoreConnection(context);
             context.BindService(new Intent(context, typeof(ServerService)), connection, Bind.None);
             return connection;
         }
@@ -54,6 +56,16 @@ namespace App.Platform.Android.Server
         public void OnServiceDisconnected(ComponentName name)
         {
             throw new Exception();
+        }
+
+        #endregion
+
+        #region Overrides of Object
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            _context.UnbindService(this);
         }
 
         #endregion
