c1c06c923820bd73964e8f4e523db24db04213cc
Roel van Uden <r.h.g.v.uden@outlook.com>
2020-05-13 20:37:29 +0200

    Fix browser navigation not throwing on request error

--- app/App.Platform.Android/Server/Plugins/Browser/BrowserViewClient.cs
+++ app/App.Platform.Android/Server/Plugins/Browser/BrowserViewClient.cs
@@ -21,31 +21,38 @@ namespace App.Platform.Android.Server.Plugins.Browser
 
         private WebResourceResponse Cache(string method, string url, IDictionary<string, string> headers)
         {
-            // Initialize the request.
-            var request = WebRequest.CreateHttp(url);
-            request.CopyCookies(CookieManager.Instance);
-            request.CopyHeaders(headers);
-            request.Method = method;
+            try
+            {
+                // Initialize the request.
+                var request = WebRequest.CreateHttp(url);
+                request.CopyCookies(CookieManager.Instance);
+                request.CopyHeaders(headers);
+                request.Method = method;
 
-            // Initialize the response.
-            using var memoryStream = new MemoryStream();
-            using var response = (HttpWebResponse) request.GetResponse();
-            using var responseStream = response.GetResponseStream();
-            responseStream?.CopyTo(memoryStream);
+                // Initialize the response.
+                using var memoryStream = new MemoryStream();
+                using var response = (HttpWebResponse) request.GetResponse();
+                using var responseStream = response.GetResponseStream();
+                responseStream?.CopyTo(memoryStream);
 
-            // Initialize the response buffer.
-            var responseBuffer = memoryStream.ToArray();
-            var responseTcs = _responseTcs.GetOrAdd(url, x => new TimeoutTaskCompletionSource<byte[]>());
-            responseTcs.TrySetResult(responseBuffer);
+                // Initialize the response buffer.
+                var responseBuffer = memoryStream.ToArray();
+                var responseTcs = _responseTcs.GetOrAdd(url, x => new TimeoutTaskCompletionSource<byte[]>());
+                responseTcs.TrySetResult(responseBuffer);
 
-            // Return the response.
-            var contentEncoding = response.ContentEncoding;
-            var contentType = Regex.Replace(response.ContentType, @"\s*;(.*)$", string.Empty);
-            var responseHeaders = response.Headers.AllKeys.ToDictionary(x => x, x => response.Headers[x]);
-            var statusCode = (int) response.StatusCode;
-            var statusDescription = response.StatusDescription;
-            var stream = new MemoryStream(responseBuffer);
-            return new WebResourceResponse(contentType, contentEncoding, statusCode, statusDescription, responseHeaders, stream);
+                // Return the response.
+                var contentEncoding = response.ContentEncoding;
+                var contentType = Regex.Replace(response.ContentType, @"\s*;(.*)$", string.Empty);
+                var responseHeaders = response.Headers.AllKeys.ToDictionary(x => x, x => response.Headers[x]);
+                var statusCode = (int) response.StatusCode;
+                var statusDescription = response.StatusDescription;
+                var stream = new MemoryStream(responseBuffer);
+                return new WebResourceResponse(contentType, contentEncoding, statusCode, statusDescription, responseHeaders, stream);
+            }
+            catch (WebException)
+            {
+                return null;
+            }
         }
 
         #endregion
@@ -80,12 +87,15 @@ namespace App.Platform.Android.Server.Plugins.Browser
 
         #region Overrides of WebViewClient
 
+        public override void OnReceivedError(WebView view, IWebResourceRequest request, WebResourceError error)
+        {
+            if (view.Url != request.Url.ToString()) return;
+            while (_visibleTcs.TryTake(out var visibleTcs)) visibleTcs.TrySetCanceled();
+        }
+
         public override void OnPageCommitVisible(WebView view, string url)
         {
-            while (_visibleTcs.TryTake(out var navigationTcs))
-            {
-                navigationTcs.TrySetResult(true);
-            }
+            while (_visibleTcs.TryTake(out var visibleTcs)) visibleTcs.TrySetResult(true);
         }
 
         public override WebResourceResponse ShouldInterceptRequest(WebView view, IWebResourceRequest request)
