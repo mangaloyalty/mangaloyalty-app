c80dea10f22d205832f86f7a7b2921853ef6f206
Deathspike <>
2020-08-22 13:51:44 +0200

    Fix missing dispose and simplify requests

--- app/App.Platform.Android/Server/Plugins/Browser/BrowserHttpRequest.cs
+++ app/App.Platform.Android/Server/Plugins/Browser/BrowserHttpRequest.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Android.Webkit;
-using App.Core.Shared;
 using App.Platform.Android.Server.Plugins.Browser.Extensions;
 
 namespace App.Platform.Android.Server.Plugins.Browser
@@ -11,64 +11,51 @@ namespace App.Platform.Android.Server.Plugins.Browser
     public class BrowserHttpRequest
     {
         private readonly HttpClient _client;
-        private readonly IDictionary<string, string> _headers;
-        private readonly TimeoutTaskCompletionSource<BrowserHttpResponse> _responseTcs;
         private readonly string _url;
-
-        #region Abstracts
-
-        private async Task RunAsync()
-        {
-            while (!_responseTcs.Task.IsCompleted)
-            {
-                try
-                {
-                    // Initialize the request.
-                    var message = new HttpRequestMessage(HttpMethod.Get, _url);
-                    message.CopyCookies(CookieManager.Instance);
-                    message.CopyHeaders(_headers, "If-Modified-Since", "If-None-Match");
-
-                    // Initialize the response.
-                    using var response = await _client.SendAsync(message, _responseTcs.CancellationToken);
-                    using var responseContent = response.Content;
-                    var responseBuffer = await responseContent.ReadAsByteArrayAsync();
-                    _responseTcs.TrySetResult(BrowserHttpResponse.Create(responseBuffer, responseContent, response));
-                }
-                catch (Exception ex)
-                {
-                    if (_responseTcs.Task.IsCompleted) return;
-                    Console.WriteLine(ex);
-                    await Task.Delay(TimeSpan.FromSeconds(1));
-                }
-            }
-        }
-
-        #endregion
 
         #region Constructor
 
-        private BrowserHttpRequest(HttpClient client, string url, IDictionary<string, string> headers)
+        public BrowserHttpRequest(HttpClient client, string url)
         {
             _client = client;
-            _headers = headers;
-            _responseTcs = new TimeoutTaskCompletionSource<BrowserHttpResponse>();
             _url = url;
         }
-
-        public static BrowserHttpRequest Create(HttpClient client, string url, IDictionary<string, string> headers)
-        {
-            var request = new BrowserHttpRequest(client, url, headers);
-            _ = request.RunAsync();
-            return request;
-        }
-
+        
         #endregion
 
         #region Methods
 
-        public async Task<BrowserHttpResponse> GetAsync()
+        public async Task<BrowserHttpResponse> GetAsync(IDictionary<string, string> headers)
         {
-            return await _responseTcs.Task;
+            using (var cancellationSource = new CancellationTokenSource(TimeSpan.FromSeconds(30)))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        // Initialize the request.
+                        var message = new HttpRequestMessage(HttpMethod.Get, _url);
+                        message.CopyHeaders(headers, "If-Modified-Since", "If-None-Match");
+                        message.CopyCookies(CookieManager.Instance);
+
+                        // Initialize the response.
+                        using var response = await _client.SendAsync(message, cancellationSource.Token);
+                        using var responseContent = response.Content;
+                        var responseBuffer = await responseContent.ReadAsByteArrayAsync();
+                        return new BrowserHttpResponse(responseBuffer, responseContent, response);
+                    }
+                    catch (HttpRequestException)
+                    {
+                        if (cancellationSource.IsCancellationRequested) throw;
+                        await Task.Delay(TimeSpan.FromSeconds(1), cancellationSource.Token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        if (cancellationSource.IsCancellationRequested) throw;
+                        await Task.Delay(TimeSpan.FromSeconds(1), cancellationSource.Token);
+                    }
+                }
+            }
         }
 
         #endregion
